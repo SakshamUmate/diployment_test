f80d414f0e27588c7f1b4dd1077b30fefb64afb2
Saksham Umate <121371463+SakshamUmate@users.noreply.github.com>
2024-01-27 23:06:33 +0530

    deployment test

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask,render_template ,request , jsonify
+
+#it is a flask aplication instance
+app=Flask(__name__)
+
+
+
+#this is for routing the home_page
+@app.route("/",methods=["GET","POST"])      
+def home_page():
+    return render_template("index.html")
+
+
+@app.route("/math",methods=["POST"])
+def calculate():
+    if (request.method=="POST"):
+        ops=(request.form["operation"])
+        n1=int(request.form["num1"])
+        n2=int(request.form["num2"])
+        
+        if ops=="add":
+            result= f"Your answer is \U0001F609 :{n1+n2}"  
+        if ops=="subtract":
+            result= f"Your answer of subtraction is \U0001F609:{n1-n2}"        
+        if ops=="multiply":
+            result= f"Your answer of multiply is \U0001F609:{n1*n2}"
+        if ops=="divide":
+            result= f"Your answer of divide is \U0001F609:{n1/n2}"
+        if ops=="Squre":
+            result= f"Your answer of Squre is \U0001F609:{n1**n2}"
+        return render_template("results.html",result=result)
+
+
+@app.route("/POSTMAN",methods=["POST"])
+def calculates():
+    if (request.method=="POST"):
+        # data extraction by json request
+        ops=(request.json["operation"])
+        n1=int(request.json["num1"])
+        n2=int(request.json["num2"])
+        
+        if ops=="add":
+            result= f"Your answer is \U0001F609 :{n1+n2}"  
+        if ops=="subtract":
+            result= f"Your answer of subtraction is \U0001F609:{n1-n2}"        
+        if ops=="multiply":
+            result= f"Your answer of multiply is \U0001F609:{n1*n2}"
+        if ops=="divide":
+            result= f"Your answer of divide is \U0001F609:{n1/n2}"
+        if ops=="x^n":
+            result= f"Your answer of n1^n2 is \U0001F609:{n1**n2}"
+        return jsonify(result)
+
+
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")
+#The host="0.0.0.0" parameter makes the application accessible from any network interface
