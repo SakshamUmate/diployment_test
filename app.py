4b7156ea161ee53ff742e130065fce9d22ca9ab8
Saksham Umate <121371463+SakshamUmate@users.noreply.github.com>
2024-01-28 02:49:15 +0530

    test

--- app.py
+++ app.py
@@ -52,5 +52,6 @@ def calculates():
         return jsonify(result)
 
 
-
+if __name__=="__main__":
+    app.run(host="0.0.0.0",port=5000,debug=True)
 #The host="0.0.0.0" parameter makes the application accessible from any network interface
